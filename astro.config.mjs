bc27904b6eeb911b385736c99f10a0ff5eca8841
jazfox <jazfox@users.noreply.github.com>
2026-07-06 17:17:20 -0700

    Fix Cloudflare Pages build dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://bloxtier.com',
-  integrations: [
-    tailwind({ applyBaseStyles: false })
-  ],
+  integrations: [],
   output: 'static'
 });
